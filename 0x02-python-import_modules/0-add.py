98f15a3a30ab588f1f546662e0fc2eb7be13463f
kwenziwa <kwenziwa@live.com>
2023-05-11 09:15:36 -0700

    update 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,6 +4,7 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    num1 = 1
-    num2 = 2
-    print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
