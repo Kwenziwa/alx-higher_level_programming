7a2caad1a1601d8fb40aee0597447d6d8e4eab53
kwenziwa <kwenziwa@live.com>
2023-05-11 08:39:45 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0_add.py
+
+if __name__ == "__main__":
+    from add_0 import add
+
+    num1 = 1
+    num2 = 2
+    print("{} + {} = {}".format(num1, num2, add(num1, num2)))
+ 
