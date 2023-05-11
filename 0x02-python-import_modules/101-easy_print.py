3b14e70465501c3419cebe75f7bcfd67c21393d3
kwenziwa <kwenziwa@live.com>
2023-05-11 13:28:16 -0700

    bug fixed

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-
-
-def magic_calculation(num1, num2):
-    if num1 < num2:
-        c = add(num1, num2)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-    else:
-        return sub(num1, num2)
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
 
