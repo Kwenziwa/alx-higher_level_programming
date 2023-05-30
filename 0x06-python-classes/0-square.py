d3007f38b6bd8bdde6c0e560c6284b279004f479
kwenziwa <kwenziwa@live.com>
2023-05-30 09:47:02 -0700

    task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,4 +5,3 @@
 class Square:
     """Square Represent."""
     pass
-
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Class Square."""
+class Square:
+
+    def __init__(self, asz):
+        self.__size = asz
