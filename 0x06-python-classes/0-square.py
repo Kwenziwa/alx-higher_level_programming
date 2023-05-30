06f71b7733e3f4b0f4d8d4ad30a26b59fd35973f
kwenziwa <kwenziwa@live.com>
2023-05-30 09:42:45 -0700

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # 0-square.py
-"""Define a class Square."""
-
+"""class Square that defines a square"""
 
 class Square:
-    """Represent a square."""
+    """Square Represent."""
     pass
+
