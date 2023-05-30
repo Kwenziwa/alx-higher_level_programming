18b0b4ffd7bcf54e1819e708f37c7c3c716b2f9a
kwenziwa <kwenziwa@live.com>
2023-05-30 10:00:29 -0700

    bugs

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,10 @@
 # 1-square.py
 
 class Square:
-    """Square represeter."""
+    """ Square represeter. """
 
     def __init__(self, size):
-        """Square Initialize .
+        """ Square Initialize .
 
         Args:
             size (int): The size of the new square.
