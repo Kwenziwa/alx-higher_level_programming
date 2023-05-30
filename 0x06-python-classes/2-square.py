0598ad2cbaf7dc87264901b75d096aef56b21fbc
kwenziwa <kwenziwa@live.com>
2023-05-30 10:12:23 -0700

    asz

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 # 2-square.py
-"""Declare a class Square."""
+""" Declare a class Square."""
 
 
 class Square:
-    """square Represent."""
+    """ square Represent."""
 
-    def __init__(self, dimension=0):
+    def __init__(self, size=0):
         """Square new Initializer .
 
         Args:
-            dimension (int): The dimension of the new square.
+            size (int): The dimension of the new square.
         """
-        if not isinstance(dimension, int):
+        if not isinstance(size, int):
             raise TypeError("The dimension must be a whole number.")
-        elif dimension < 0:
+        elif size < 0:
             raise ValueError("dimension must be greater or equal 0")
-        self.__size = dimension
+        self.__size = size
