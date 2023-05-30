93bb6efd2f8b836edf774a3e7c442ac31d55afab
kwenziwa <kwenziwa@live.com>
2023-05-30 10:21:09 -0700

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 # 3-square.py
-"""Define a class Square."""
-
 class Square:
-
+    """ Declare Square class"""
     def __init__(self, size=0):
-       
-        if not isinstance(size, int):
-            raise TypeError("size should be an int")
+        if type(size) != int:
+            raise TypeError("size should be integer")
         elif size < 0:
-            raise ValueError("size is not >= 0")
-        self.__size = size
+            raise ValueError("size should be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-       
-        return (self.__size * self.__size)
+        return self.__size * self.__size
