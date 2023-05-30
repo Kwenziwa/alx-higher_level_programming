056c5582cc9e9eec28d2dbc3914fe9529349f250
kwenziwa <kwenziwa@live.com>
2023-05-30 12:23:00 -0700

    errors

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # 3-square.py
+
 class Square:
     """ Declare Square class"""
     def __init__(self, dms=0):
+
         if type(dms) != int:
             raise TypeError("size must be an integer")
         elif dms < 0:
