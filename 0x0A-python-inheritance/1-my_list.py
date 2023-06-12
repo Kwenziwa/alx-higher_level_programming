a0292eb5c817cbb56c588a1d901e3cbe4848ac1e
kwenziwa <kwenziwa@live.com>
2023-06-12 09:40:09 -0700

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+# 1-my_list.py
+"""Class MyList defines an inherited list ."""
+class MyList(list):
+    """printing for the built-in list class Implemented."""
+    def print_sorted(self):
+        """Printing a list in sorted in a ascending order."""
+        print(sorted(self))
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
