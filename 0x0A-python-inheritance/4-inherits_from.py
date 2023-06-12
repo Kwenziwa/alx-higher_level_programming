d0cb64f29a7f7a13cc53e719127452f78d09f166
kwenziwa <kwenziwa@live.com>
2023-06-12 10:11:11 -0700

    bug fixed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # 4-inherits_from.py
+#!/usr/bin/python3
+# 4-inherits_from.py
 """
 ===================================
  A defined class and inherited class-checking function.
@@ -7,4 +9,10 @@
 """
 
 def inherits_from(obj, a_class):
+    """
+    return True if an object is an instance of a class
+    that inherited from
+    """
+
     return False if type(obj) is a_class else isinstance(obj, a_class)
+
