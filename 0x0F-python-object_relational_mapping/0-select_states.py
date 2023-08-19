1ad41be9ba5e5eb926ae2d010b2d9863d3dfdc6b
kwenziwa <kwenziwa@live.com>
2023-08-18 18:38:00 -0700

    error fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
 # Lists all states from the database hbtn_0e_0_usa.
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    data = cursor.fetchall()
+def list_states(username, password, db_name):
 
-    for row in data:
+    # Connect to the MySQL server
+    db_conn = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=db_name
+    )
+    cursor = db_conn.cursor()
+    sql_query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(sql_query)
+
+    # Fetch all the all_states
+    all_states = cursor.fetchall()
+
+    # Display the all_states
+    for row in all_states:
         print(row)
 
+    # Close the cursor and connection
     cursor.close()
-    db.close()
+    db_conn.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <db_name>")
+        sys.exit(1)
+
+    user_name = sys.argv[1]
+    pword = sys.argv[2]
+    db_name = sys.argv[3]
+
+    list_states(user_name, pword, db_name)
+
