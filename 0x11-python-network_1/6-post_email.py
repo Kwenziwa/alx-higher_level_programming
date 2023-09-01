a12183f7d2eb7da6d9cc5b31dd285d62c6c4c847
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-01 11:52:30 -0400

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
-"""
+"""Write a Python script that takes in a URL and an email address..."""
 import sys
 import requests
 
