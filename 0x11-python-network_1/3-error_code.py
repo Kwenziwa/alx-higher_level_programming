e1af39efbeef41a25b2ef9ea17ec8544e2fcd961
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-01 11:51:45 -0400

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""sends a request to the URL and displays the body of the response.
-
-Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
-"""
+"""sends a request to the URL and displays the body of the responses"""
 import sys
 import urllib.error
 import urllib.request
