c9a725e7cf9fe45c0b32901a3efa4a53c108b503
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-26 15:05:48 -0400

    Update 2-statuscode.js bug

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const the_request = require("the_request");
+const the_request = require("request");
 the_request.get(process.argv[2]).on("response", function (response) {
   console.log(`code: ${response.statusCode}`);
 });
