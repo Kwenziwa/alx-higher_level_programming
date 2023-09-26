408f0702b13073ee83caf4759a84967a156be1fe
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-26 15:31:16 -0400

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,25 +1,16 @@
 #!/usr/bin/node
 
 const the_request = require("request");
+const epsNumber = process.argv[2];
+const apiURL = "https://swapi-api.hbtn.io/api/films/";
 
-const movieId = process.argv[2];
-const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-
-if (!movieId) {
-  console.error("Usage: node get_star_wars_movie.js <movie_id>");
-  process.exit(1);
-}
-
-the_request(apiURL, (error, response, body) => {
-  if (error) {
-    console.error(`An error occurred: ${error}`);
+the_request(apiURL + epsNumber, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
   } else {
-    if (response.statusCode === 200) {
-      const movieData = JSON.parse(body);
-      console.log(`Title: ${movieData.title}`);
-    } else {
-      console.error(`request failed with status code ${response.statusCode}`);
-    }
+    console.log("Error code: " + response.statusCode);
   }
 });
