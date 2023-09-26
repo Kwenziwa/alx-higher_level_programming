4f0e3e5157bf17b4b0470cd5ec71a45d3896e6d0
Kwenziwa Lizwi <Kwenziwa@live.com>
2023-09-26 15:02:12 -0400

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
-
-const fs = require("fs");
-
-const theFilePath = process.argv[2];
-const theContentToWrite = process.argv[3];
-
-if (!theFilePath || !theContentToWrite) {
-  console.error('Usage: node write_file.js <file_path> "<content_to_write>"');
-  process.exit(1);
-}
-
-fs.writeFile(theFilePath, theContentToWrite, "utf-8", (error) => {
-  if (error) {
-    console.error(`An error occurred while writing to the file: ${error}`);
-  } else {
-    console.log(`Content has been successfully written to ${theFilePath}`);
-  }
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
-
